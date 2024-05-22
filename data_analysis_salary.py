f3472326548e2bdafcf3791f85d4022aadacf1de
monicaarghir4 <marghir4@gmail.com>
2024-05-22 13:22:46 +0300

    3.1 Exploratory Data Analysis

--- data_analysis_salary.py
+++ data_analysis_salary.py
@@ -2,66 +2,149 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
+import seaborn as sns
+from scipy.stats import chi2_contingency
 
-# Load the data
+# Incarcarea datelor
 data = pd.read_csv('tema2_SalaryPrediction/SalaryPrediction_full.csv')
 
-# split the columns into the numerical ones and the other
+# Impartirea datelor in cele numerice si cele categorice
 numerical = data.select_dtypes(include=[np.number])
 categorical = data.select_dtypes(exclude=[np.number])
 
+# Statistica datelor numerice
 stats_numerical = numerical.describe().T
 stats_numerical['count_no_missing'] = numerical.notnull().sum()
-stats_numerical.rename(columns={'50%': '50% (Median)'}, inplace=True)
 
-stats_numerical = stats_numerical[['count_no_missing', 'mean', 'std', 'min', '25%', '50% (Median)', '75%', 'max']]
+# Redenumirea coloanelor
+stats_numerical = stats_numerical[['count_no_missing', 'mean', 'std', 'min', '25%', '50%',
+                                   '75%', 'max']]
 stats_numerical.columns = [
-    'Nr. de exemple fără valori lipsă', 'Valoarea medie', 'Deviația standard',
-    'Valoarea minimă', 'Percentila de 25%', 'Percentila de 50%',
-    'Percentila de 75%', 'Valoarea maximă'
+    'Nr. de exemple fara valori lipsa', 'Valoarea medie', 'Deviatia standard',
+    'Valoarea minima', 'Percentila de 25%', 'Percentila de 50%',
+    'Percentila de 75%', 'Valoarea maxima'
 ]
 
+# Rotunjirea datelor la 2 zecimale
 stats_numerical = stats_numerical.round(2)
 
-# Create a PrettyTable object
+# Creare tabel in format pretty
 table_numerical = PrettyTable()
 table_numerical.field_names = ["Atribut"] + list(stats_numerical.columns)
 
 for row in stats_numerical.itertuples():
     table_numerical.add_row([row.Index] + list(row[1:]))
 
-# Print the nicely formatted table
 print(table_numerical)
 
+# Creare grafice boxplot pentru fiecare atribut
 for column in numerical.columns:
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(6, 6))
     numerical.boxplot(column=column, grid=False)
     plt.title(f'Boxplot pentru {column}')
+    plt.ylabel('Valoare')
     plt.show()
 
-# Categorical data
+# Statistica datelor categorice
 stats_categorical = pd.DataFrame({
-    'Număr de exemple fără valori lipsă': categorical.notnull().sum(),
-    'Număr de valori unice': categorical.nunique()
+    'Numar de exemple fara valori lipsa': categorical.notnull().sum(),
+    'Numar de valori unice': categorical.nunique()
 })
 
-# Create a PrettyTable object
+# Afisare tabel in format pretty
 table_categorical = PrettyTable()
 table_categorical.field_names = ["Atribut"] + list(stats_categorical.columns)
 
 for row in stats_categorical.itertuples():
     table_categorical.add_row([row.Index] + list(row[1:]))
 
-# Print the nicely formatted table
 print(table_categorical)
 
-# Plot the histograms
+# Crearea histogramelor pentru atribute
 for column in categorical.columns:
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(10, 10))
     categorical[column].hist(grid=False, bins=30, figsize=(10,6))
-    plt.title(f'Histogramă pentru {column}')
+    plt.title(f'Histograma pentru {column}')
     plt.xlabel(column)
-    plt.ylabel('Frecvență')
+    plt.ylabel('Frecventa')
     plt.xticks(rotation=90)
     plt.show()
 
+##################################################################################################
+
+# Incarcarea datelor de antrenare si de testare
+data_train = pd.read_csv('tema2_SalaryPrediction/SalaryPrediction_train.csv')
+data_test = pd.read_csv('tema2_SalaryPrediction/SalaryPrediction_test.csv')
+
+# Extragerea datelor categorice de antrenare si testare
+train_categorical = data_train.select_dtypes(exclude=[np.number])
+test_categorical = data_test.select_dtypes(exclude=[np.number])
+
+# Crearea graficelor de tip barplot pentru fiecare atribut categoric
+# din setul de antrenare
+for column in train_categorical.columns:
+    plt.figure(figsize=(10, 10))
+
+    train_counts = train_categorical[column].value_counts().reset_index()
+    train_counts.columns = [column, 'Frecventa']
+
+    sns.barplot(x=column, y='Frecventa', data=train_counts)
+
+    plt.title(f'Barplot pentru antrenare {column}')
+    plt.xlabel(column)
+    plt.ylabel('Frecventa')
+    plt.xticks(rotation=90)
+    plt.show()
+
+# Crearea graficelor de tip barplot pentru fiecare atribut categoric
+# din setul de testare
+for column in test_categorical.columns:
+    plt.figure(figsize=(10, 10))
+
+    test_counts = test_categorical[column].value_counts().reset_index()
+    test_counts.columns = [column, 'Frecventa']
+
+    sns.barplot(x=column, y='Frecventa', data=test_counts)
+
+    plt.title(f'Barplot pentru testare {column}')
+    plt.xlabel(column)
+    plt.ylabel('Frecventa')
+    plt.xticks(rotation=90)
+    plt.show()
+
+##################################################################################################
+
+# Matrice de corelatie pentru atributele numerice
+corr_matrix = numerical.corr(method='pearson')
+plt.figure(figsize=(15, 15))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+plt.title('Matrice de corelatie pentru atributele numerice din datasetul Salary')
+plt.yticks(rotation=0)
+plt.xticks(rotation=0)
+plt.show()
+
+# Testul chi2 pentru atributele categorice
+def chi2_test(data_frame, col1, col2):
+    contingency_table = pd.crosstab(data_frame[col1], data_frame[col2])
+    result = chi2_contingency(contingency_table)
+    return result[1]
+
+# Matrice de p-values pentru atributele categorice
+categorical_columns = categorical.columns
+p_values = pd.DataFrame(index=categorical_columns, columns=categorical_columns)
+
+# Completarea matricei cu 1 pe diagonala principala si cu valoarea testului chi2 in rest
+for col1 in categorical_columns:
+    for col2 in categorical_columns:
+        if col1 != col2:
+            p_values.loc[col1, col2] = chi2_test(categorical, col1, col2)
+        else:
+            p_values.loc[col1, col2] = 1.0
+
+# Afisare unei figuri pentru matricea obtinuta
+plt.figure(figsize=(15,15))
+sns.heatmap(p_values.astype(float), annot=True, cmap='coolwarm', vmin=0, vmax=1)
+plt.title('Matrice de p-values pentru atributele categorice din datasetul Salary')
+plt.yticks(rotation=0)
+plt.xticks(rotation=45)
+plt.show()
